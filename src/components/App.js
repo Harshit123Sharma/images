f47be1e0b4730a42f26c661af04e8a8d23fd4f87
harshit2001sharma@gmail.com <44293679+Harshit123Sharma@users.noreply.github.com>
2021-05-25 22:50:21 +0530

    Added searchbar and made api request

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
 import React from "react";
+import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
 
+class App extends React.Component {
 
-const App = () => {
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+  const response = await unsplash.get("/search/photos", {
+    params: { query: term }
+  });
+
+  this.setState({ images: response.data.results });
+  
+  /*.then(response => {
+    console.log(response.data.results);
+  });*/
+  }
+
+  render () {
     return (
-    <div className="ui container" style={{marginTop : "10px"}}>
-        <SearchBar />
-    </div>
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
+      </div>
     );
-};
+  }
+      
+  
+  }
+
 
 export default App;
